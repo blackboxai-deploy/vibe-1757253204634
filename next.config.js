609cacdaf2395163a0a1a8ad8117938287acefdd
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-07 19:23:37 +0530

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      }
+    ],
+  },
+};
+
+module.exports = nextConfig;
